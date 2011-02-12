cb5ffe47c9ea05308b8ec8d71b12b41b77c0e2f5
Arian Glander <glander@me.com>
2011-02-12 01:47:22 +0100

    rename

--- js/wyrian.js
+++
@@ -1,9 +0,0 @@
-var Wyrian = function() {
-	// do basic initializations
-	
-	var inputController = new InputController(),
-		displayController = new DisplayController(),
-		gameController = new GameController(inputController, displayController);
-
-	gameController.start();
-}();
